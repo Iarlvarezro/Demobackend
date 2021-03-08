5aec055162269c8190e3660f4ac574cf0308e0ff
Iarlvarezro <psialvarezro@gmail.com>
2021-03-08 19:43:42 +0100

    second commit

--- src/main/java/com/example/demo/application/commentapplication/CommentApplication.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.application.commentapplication;
-
-import com.example.demo.dto.commentdtos.CommentDTO;
-import com.example.demo.dto.commentdtos.CreateCommentDTO;
-
-public interface CommentApplication {
-    public CommentDTO add(CreateCommentDTO dto);
-}
